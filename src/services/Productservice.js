8cbb8b52149510d17ac115b65653c059ae31a245
DHANAVANTH <122664020+DHANAVANTH@users.noreply.github.com>
2026-03-05 09:26:59 +0530

    Update ProductService API endpoints for products

--- src/services/Productservice.js
+++ src/services/Productservice.js
@@ -4,23 +4,23 @@ const BASE_URL = "/api/v1";
 
 class ProductService {
   getProducts() {
-    return axios.get(`${BASE_URL}/stlist`);
+    return axios.get(`${BASE_URL}/ptlist`);
   }
 
   getProductById(id) {
-    return axios.get(`${BASE_URL}/students/${id}`);
+    return axios.get(`${BASE_URL}/products/${id}`);
   }
 
   addproduct(product) {
-    return axios.post(`${BASE_URL}/insertStudent`, product);
+    return axios.post(`${BASE_URL}/insertproduct`, product);
   }
 
   updateProduct(id, products) {
-    return axios.post(`${BASE_URL}/updatestudent/${id}`, products);
+    return axios.post(`${BASE_URL}/updateproduct/${id}`, products);
   }
 
   deleteProduct(id) {
-    return axios.delete(`${BASE_URL}/deletestudent/${id}`);
+    return axios.delete(`${BASE_URL}/deleteproduct/${id}`);
   }
 }
 
